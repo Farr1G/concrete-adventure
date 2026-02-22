9cd67930edc851e24479c09898de4e24d0a8189e
Farr1G <OkayButrio@yandex.ru>
2026-02-22 23:22:33 +0300

    Добавил полезные ссылки (скрытый файл), косметические изменения меню

--- main.lua
+++ main.lua
@@ -2,12 +2,15 @@
 
 -- variables
 
+-- Redo by using https://github.com/hoelzro/lua-term
+-- http://lua-users.org/wiki/AnsiTerminalColors
 local COLORS = {
     RED = "\27[31m",
     GREEN = "\27[32m",
     YELLOW = "\27[33m",
     BLUE = "\27[34m",
-    GREY = "\27[30m",
+
+    DIM = "\27[2m",
     
     RESET = "\27[0m",
 }
@@ -16,31 +19,38 @@ local function clear_screen()
     os.execute("clear")
 end
 
-local function wait_for_input(isNumber)
+local function wait_for_input()
     io.write(COLORS.YELLOW.."\n> "..COLORS.RESET); io.flush()
-    return io.read(isNumber and "n")
+    return io.read()
+end
+
+local function print_divisor()
+    print(COLORS.DIM..string.rep("=", 50)..COLORS.RESET)
 end
 
 -- TODO: Add logic
 local function PrintMenu()
     clear_screen()
-    print(COLORS.GREY..string.rep("=", 50)..COLORS.RESET)
+    print_divisor()
+    print("")
 
-    print("Бетонное приключение "..COLORS.RED.."[В разработке]"..COLORS.RESET)
+    print("Бетонное приключение "..COLORS.YELLOW.."[В разработке]"..COLORS.RESET)
     print("Сделано Платоном Акуловым")
 
     print("\n1. Новая игра")
     print("2. Загрузить сохранение")
     print("3. Настройки")
     print("4. Об игре")
+    print("5. Выйти")
 
-    print(COLORS.GREY..string.rep("=", 50)..COLORS.RESET)
+    print("")
+    print_divisor()
 end
 
-local chosenOption = ""
+local chosenOption = 0
 while true do
     PrintMenu()
-    chosenOption = wait_for_input("n")
+    chosenOption = tonumber(wait_for_input())
 
     -- Just a test
     if chosenOption == 1 then
